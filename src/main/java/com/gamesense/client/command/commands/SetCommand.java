597b36db467da906156f2dcd111fc1a6fedc4626
TechAle <alessandro.condello.email@gmail.com>
2021-07-02 17:44:08 +0200

    Now set command works

--- src/main/java/com/gamesense/client/command/commands/SetCommand.java
+++ src/main/java/com/gamesense/client/command/commands/SetCommand.java
@@ -15,20 +15,20 @@ import com.gamesense.client.module.ModuleManager;
  * @Ported and modified on 11/05/2020
  */
 
-@Command.Declaration(name = "Set", syntax = "set [module] [setting] value (no color support)", alias = {"set", "setmodule", "changesetting", "setting"})
+@Command.Declaration(name = "Set", syntax = "set [module] [setting] value (no color support) module space = _ + remove setting space", alias = {"set", "setmodule", "changesetting", "setting"})
 public class SetCommand extends Command {
 
     public void onCommand(String command, String[] message) {
         String main = message[0];
 
-        Module module = ModuleManager.getModule(main);
+        Module module = ModuleManager.getModule(main.replace("_", " "));
 
         if (module == null) {
             MessageBus.sendCommandMessage(this.getSyntax(), true);
             return;
         }
 
-        SettingsManager.getSettingsForModule(module).stream().filter(setting -> setting.getConfigName().equalsIgnoreCase(message[1])).forEach(setting -> {
+        SettingsManager.getSettingsForModule(module).stream().filter(setting -> setting.getConfigName().equalsIgnoreCase(message[1].replace("_", " "))).forEach(setting -> {
             if (setting instanceof BooleanSetting) {
                 if (message[2].equalsIgnoreCase("true") || message[2].equalsIgnoreCase("false")) {
                     setting.setValue(Boolean.parseBoolean(message[2]));
@@ -37,26 +37,10 @@ public class SetCommand extends Command {
                     MessageBus.sendCommandMessage(this.getSyntax(), true);
                 }
             } else if (setting instanceof IntegerSetting) {
-                if (Integer.parseInt(message[2]) > ((IntegerSetting) setting).getMax()) {
-                    setting.setValue(((IntegerSetting) setting).getMax());
-                }
-                if (Integer.parseInt(message[2]) < ((IntegerSetting) setting).getMin()) {
-                    setting.setValue(((IntegerSetting) setting).getMin());
-                }
-                if (Integer.parseInt(message[2]) < ((IntegerSetting) setting).getMax() && Integer.parseInt(message[2]) > ((IntegerSetting) setting).getMin()) {
-                    setting.setValue(Integer.parseInt(message[2]));
-                }
+                setting.setValue(Integer.parseInt(message[2]));
                 MessageBus.sendCommandMessage(module.getName() + " " + setting.getConfigName() + " set to: " + setting.getValue() + "!", true);
             } else if (setting instanceof DoubleSetting) {
-                if (Double.parseDouble(message[2]) > ((DoubleSetting) setting).getMax()) {
-                    setting.setValue(((DoubleSetting) setting).getMax());
-                }
-                if (Double.parseDouble(message[2]) < ((DoubleSetting) setting).getMin()) {
-                    setting.setValue(((DoubleSetting) setting).getMin());
-                }
-                if (Double.parseDouble(message[2]) < ((DoubleSetting) setting).getMax() && Double.parseDouble(message[2]) > ((DoubleSetting) setting).getMin()) {
-                    setting.setValue(Double.parseDouble(message[2]));
-                }
+                setting.setValue(Double.parseDouble(message[2]));
                 MessageBus.sendCommandMessage(module.getName() + " " + setting.getConfigName() + " set to: " + setting.getValue() + "!", true);
             } else if (setting instanceof ModeSetting) {
                 if (!((ModeSetting) setting).getModes().contains(message[2])) {
