6a94ffdd9bd847f8a48011f70bb60a35695b0b0e
Doogie13 <jay.finbow13@gmail.com>
2021-07-14 21:56:10 +0100

    Added silentSwitch and onGround

--- src/main/java/com/gamesense/client/module/modules/misc/MCP.java
+++ src/main/java/com/gamesense/client/module/modules/misc/MCP.java
@@ -4,16 +4,20 @@ import com.gamesense.api.setting.values.BooleanSetting;
 import com.gamesense.api.setting.values.IntegerSetting;
 import com.gamesense.api.setting.values.ModeSetting;
 import com.gamesense.api.util.player.InventoryUtil;
-import com.gamesense.api.util.world.BlockUtil;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemEnderPearl;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Mouse;
+
 
 import java.util.Arrays;
 
@@ -27,6 +31,8 @@ public class MCP extends Module {
     ModeSetting button = registerMode("Button", Arrays.asList("MOUSE3", "MOUSE4", "MOUSE5"), "MOUSE3");
     BooleanSetting clipRotate = registerBoolean("clipRotate", false);
     IntegerSetting pearlPitch = registerInteger("Pitch", 85, -90, 90);
+    BooleanSetting onGroundCheck = registerBoolean("onGround", true);
+    BooleanSetting silentSwitch = registerBoolean("silentSwitch", false);
 
     public void onUpdate() {
 
@@ -43,21 +49,20 @@ public class MCP extends Module {
             MCPButtonCode = 2; //User Error Protection
         }
 
-        if (Mouse.isButtonDown(MCPButtonCode)) { //We check for button press and don't check for miss :rage:
+        if ((Mouse.isButtonDown(MCPButtonCode) && onGroundCheck.getValue() && mc.player.onGround) || Mouse.isButtonDown(MCPButtonCode) && !onGroundCheck.getValue() ) { //We check for button press and don't check for miss :rage:
             int oldSlot = mc.player.inventory.currentItem;
 
             int pearlSlot = InventoryUtil.findFirstItemSlot(ItemEnderPearl.class, 0, 8);
 
             if (pearlSlot != -1) {
+                if (!silentSwitch.getValue()) {
+                    mc.player.inventory.currentItem = pearlSlot;
 
-                mc.player.inventory.currentItem = pearlSlot;
+                }else{
+                    mc.player.connection.sendPacket(new CPacketHeldItemChange(pearlSlot));
+                }
 
                 if (clipRotate.getValue()){
-
-                    //Get old yaw so we can return it back to before as sometimes glitches idk
-
-                    float oldYaw = mc.player.rotationYaw;
-                    float oldPitch = mc.player.rotationPitch;
 
                     //ROUNDING
                     float yawRounded;
@@ -72,14 +77,20 @@ public class MCP extends Module {
                     }
                     //END OF ROUNDING
 
+                    //new rotate
+
+                    //mc.player.setPositionAndRotationDirect(mc.player.posX,mc.player.posY,mc.player.posZ,yawRounded,pearlPitchFloat,1,true);
+
                     mc.player.connection.sendPacket(new CPacketPlayer.Rotation(yawRounded, pearlPitchFloat, true)); // rotate for phasing
                     mc.playerController.processRightClick(mc.player, mc.world, EnumHand.MAIN_HAND); // Throw the pearl (thanks TechAle :D)
 
-                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(oldYaw, oldPitch, true)); // rotate back
+                    // mc.player.connection.sendPacket(new CPacketPlayer.Rotation(oldYaw, oldPitch, true)); // rotate back (disabled)
                     mc.player.inventory.currentItem = oldSlot; // return to old slot
                 }else{ // same as Hoosiers' code previous code
                     mc.playerController.processRightClick(mc.player, mc.world, EnumHand.MAIN_HAND);
-                    mc.player.inventory.currentItem = oldSlot;
+                    if (!silentSwitch.getValue()){
+                        mc.player.inventory.currentItem = oldSlot;
+                    }
                 }
             }
         }
