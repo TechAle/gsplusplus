41f91c3bfce9cef57892fbde49d070ff9e574b0c
TechAle <alessandro.condello.email@gmail.com>
2021-07-17 03:21:17 +0200

    Fixed fastBow and quiver being friends and dumb

--- src/main/java/com/gamesense/client/module/modules/combat/FastBow.java
+++ src/main/java/com/gamesense/client/module/modules/combat/FastBow.java
@@ -3,6 +3,7 @@ package com.gamesense.client.module.modules.combat;
 import com.gamesense.api.setting.values.IntegerSetting;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
+import com.gamesense.client.module.ModuleManager;
 import net.minecraft.item.ItemBow;
 import net.minecraft.network.play.client.CPacketPlayerDigging;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
@@ -14,7 +15,7 @@ public class FastBow extends Module {
     IntegerSetting drawLength = registerInteger("Draw Length", 3, 0, 21);
 
     public void onUpdate() {
-        if (mc.player.getHeldItemMainhand().getItem() instanceof ItemBow && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= drawLength.getValue()) {
+        if (mc.player.getHeldItemMainhand().getItem() instanceof ItemBow && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= drawLength.getValue() && ModuleManager.isModuleEnabled("Quiver")) {
             mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, mc.player.getHorizontalFacing()));
             mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(mc.player.getActiveHand()));
             mc.player.stopActiveHand();
