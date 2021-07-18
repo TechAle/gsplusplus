645366e759a9f0e34a104fd427dcdd45b86a6718
Doogie13 <jay.finbow13@gmail.com>
2021-07-18 20:54:04 +0100

    attempted to add phase

--- src/main/java/com/gamesense/client/module/modules/combat/FootConcrete.java
+++ src/main/java/com/gamesense/client/module/modules/combat/FootConcrete.java
@@ -40,6 +40,7 @@ public class FootConcrete extends Module {
     BooleanSetting allowEchest = registerBoolean("allowEchest", true);
     BooleanSetting onlyEchest = registerBoolean("onlyEchest", false, () -> allowEchest.getValue());
     BooleanSetting silentSwitch = registerBoolean("silentSwitch", true, () -> jumpMode.getValue().equals("real"));
+    BooleanSetting phase = registerBoolean("clipDown", false);
     BooleanSetting rotate = registerBoolean("rotate", true);
 
     boolean doGlitch;
@@ -157,6 +158,11 @@ public class FootConcrete extends Module {
                     }
 
                     mc.player.connection.sendPacket(new CPacketHeldItemChange(oldslot));
+
+                    if (phase.getValue()) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX + mc.player.motionX, mc.player.posY - 0.0624, mc.player.posZ + mc.player.motionZ, mc.player.rotationYaw, mc.player.rotationPitch, false));
+                        mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX + mc.player.motionX, mc.player.posY - 42069, mc.player.posZ + mc.player.motionZ, mc.player.rotationYaw , mc.player.rotationPitch, true));
+                    }
 
                     disable();
 
