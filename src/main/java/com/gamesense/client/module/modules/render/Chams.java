137460af2aae0d90a3b6543b570161219c2bcdf4
TechAle <alessandro.condello.email@gmail.com>
2021-07-26 15:12:26 +0200

    Merged chams and popChams, added gradientAlpha

--- src/main/java/com/gamesense/client/module/modules/render/Chams.java
+++ src/main/java/com/gamesense/client/module/modules/render/Chams.java
@@ -1,21 +1,33 @@
 package com.gamesense.client.module.modules.render;
 
 import com.gamesense.api.event.events.RenderEntityEvent;
+import com.gamesense.api.event.events.TotemPopEvent;
 import com.gamesense.api.setting.values.*;
 import com.gamesense.api.util.render.ChamsUtil;
 import com.gamesense.api.util.render.GSColor;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
+import com.mojang.authlib.GameProfile;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
+import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.GameType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Techale
@@ -36,6 +48,91 @@ public class Chams extends Module {
     ColorSetting playerColor = registerColor("Player Color", new GSColor(0, 255, 255, 255));
     ColorSetting mobColor = registerColor("Mob Color", new GSColor(255, 255, 0, 255));
     ColorSetting crystalColor = registerColor("Crystal Color", new GSColor(0, 255, 0, 255));
+    BooleanSetting chamsPop = registerBoolean("Chams Pop", false);
+    ModeSetting chamsPopType = registerMode("Chams Pop Type", Arrays.asList("Color", "WireFrame"), "WireFrame");
+    ColorSetting chamsColor = registerColor("Chams Color", new GSColor(255, 255, 255, 255));
+    IntegerSetting wireFramePop = registerInteger("WireFrame Pop", 4, 0, 10);
+    BooleanSetting gradientAlpha = registerBoolean("Gradient Alpha", true);
+
+    IntegerSetting life = registerInteger("Time", 100, 10, 300);
+
+
+    private int fpNum = 0;
+    List<Integer> fakePlayers = new ArrayList<>();
+
+    ArrayList<Entity> toSpawn = new ArrayList<>();
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    private final Listener<TotemPopEvent> totemPopEventListener = new Listener<>(event -> {
+        if (chamsPop.getValue())
+            toSpawn.add(event.getEntity());
+    });
+
+    @Override
+    public void onUpdate() {
+
+        if (mc.world == null || mc.player == null)
+            toSpawn.clear();
+
+        toSpawn.removeIf(this::spawnPlayer);
+
+        for(int i = 0; i < fakePlayers.size(); i++) {
+
+            try {
+                if (mc.world.getEntityByID(fakePlayers.get(i)).ticksExisted > life.getValue()) {
+                    mc.world.removeEntityFromWorld(fakePlayers.get(i));
+                    fakePlayers.remove(fakePlayers.get(i));
+                    i--;
+                }
+            }catch (NullPointerException e) {
+                fakePlayers.remove(fakePlayers.get(i));
+                i--;
+            }
+        }
+
+
+    }
+
+
+    boolean spawnPlayer(Entity entity) {
+        // Clone empty player
+        EntityOtherPlayerMP clonedPlayer = new EntityOtherPlayerMP(mc.world, new GameProfile(UUID.fromString("fdee323e-7f0c-4c15-8d1c-0f277442342a"), ""));
+        // Copy angles
+        clonedPlayer.copyLocationAndAnglesFrom(entity);
+        clonedPlayer.rotationYawHead = entity.getRotationYawHead();
+        clonedPlayer.rotationYaw = entity.rotationYaw;
+        clonedPlayer.rotationPitch = entity.rotationPitch;
+        /// Trying to make others ca not target this
+        // idk maybe some ca not considerate spectator
+        clonedPlayer.setGameType(GameType.SPECTATOR);
+        clonedPlayer.setHealth(20);
+        // Add resistance for 0 damage
+        clonedPlayer.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 100, 100, false, false));
+        // Add armor for not making some ca target this because "naked"
+        final ItemStack[] armors = new ItemStack[]{
+                new ItemStack(Items.DIAMOND_BOOTS),
+                new ItemStack(Items.DIAMOND_LEGGINGS),
+                new ItemStack(Items.DIAMOND_CHESTPLATE),
+                new ItemStack(Items.DIAMOND_HELMET)
+        };
+        for (int i = 0; i < 4; i++) {
+            // Create base
+            ItemStack item = armors[i];
+            // Add enchants
+            item.addEnchantment(
+                    i == 2 ? Enchantments.BLAST_PROTECTION : Enchantments.PROTECTION,
+                    50);
+            // Add it to the player
+            clonedPlayer.inventory.armorInventory.set(i, item);
+        }
+        // Add entity id
+        mc.world.addEntityToWorld((-1235 - fpNum), clonedPlayer);
+        clonedPlayer.onLivingUpdate();
+        fakePlayers.add((-1235 - fpNum));
+        fpNum++;
+        return true;
+    }
 
     @SuppressWarnings("unused")
     @EventHandler
@@ -57,6 +154,9 @@ public class Chams extends Module {
         }
 
         if (player.getValue() && entity1 instanceof EntityPlayer && entity1 != mc.player) {
+            if ( chamsPop.getValue() && entity1.getName().length() == 0) {
+                renderChamsPop(entity1);
+            } else
             renderChamsPre(new GSColor(playerColor.getValue(), 255), true);
         }
 
@@ -113,6 +213,28 @@ public class Chams extends Module {
             }
             case "WireFrame": {
                 ChamsUtil.createWirePre(new GSColor(color, wireOpacity.getValue()), lineWidth.getValue(), isPlayer);
+                break;
+            }
+        }
+    }
+
+    private void renderChamsPop(Entity player) {
+
+        int alpha = chamsColor.getColor().getAlpha();
+        if (gradientAlpha.getValue()) {
+            alpha = (int) (250 - (player.ticksExisted / (float) life.getValue() * 250));
+        }
+
+        if (alpha < 0)
+            alpha = 0;
+
+        switch (chamsPopType.getValue()) {
+            case "Color": {
+                ChamsUtil.createColorPre(new GSColor(chamsColor.getColor(), alpha), true);
+                break;
+            }
+            case "WireFrame": {
+                ChamsUtil.createWirePre(new GSColor(chamsColor.getColor(), alpha), wireFramePop.getValue(), true);
                 break;
             }
         }
