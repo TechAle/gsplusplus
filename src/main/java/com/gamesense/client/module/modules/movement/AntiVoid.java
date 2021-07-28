a4a2482763a1e25a5c194e68f5695e6e7c1563b3
Doogie13 <jay.finbow13@gmail.com>
2021-07-28 18:04:59 +0100

    antivoid > catch causes invalid hotbar??? what the fuck?????

--- src/main/java/com/gamesense/client/module/modules/movement/AntiVoid.java
+++ src/main/java/com/gamesense/client/module/modules/movement/AntiVoid.java
@@ -9,10 +9,12 @@ import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
 import com.gamesense.client.module.ModuleManager;
 import com.gamesense.client.module.modules.gui.ColorMain;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.Vec3d;
 
 import java.lang.reflect.Array;
@@ -22,33 +24,56 @@ import java.util.Arrays;
 public class AntiVoid extends Module {
 
     ModeSetting mode = registerMode("Mode", Arrays.asList("Freeze", "Catch"), "Freeze");
+    DoubleSetting fheight = registerDouble("Freeze Height", 2,0,5);
     DoubleSetting height = registerDouble("Catch Height", 2,0,5);
 
     boolean doPlace;
+    int targetBlockSlot;
+    boolean cont;
 
     @Override
     public void onUpdate() {
-        if (!mc.player.noClip /*to account for packetfly*/ && mc.player.posY < 0 && mode.getValue().equalsIgnoreCase("Freeze")) {
+        if (!mc.player.noClip /*to account for packetfly*/ && mc.player.posY < fheight.getValue() && mode.getValue().equalsIgnoreCase("Freeze") && mc.world.getBlockState(new BlockPos(mc.player.posX,1,mc.player.posZ)).getMaterial().isReplaceable()) {
 
             mc.player.setVelocity(0.0D, 0.0D, 0.0D);
 
             if (mc.player.getRidingEntity() != null)
                 mc.player.getRidingEntity().setVelocity(0.0D, 0.0D, 0.0D);
-        } else if (!mc.player.noClip /*to account for packetfly*/ && mc.player.posY < height.getValue() && mode.getValue().equalsIgnoreCase("Catch") && (mc.world.getBlockState(new BlockPos(mc.player.posX,0, mc.player.posZ)).getMaterial().isReplaceable())) {
+        } else if (!mc.player.noClip /*to account for packetfly*/ && mc.player.posY < height.getValue() && mode.getValue().equalsIgnoreCase("Catch")) {
 
-                placeBlockPacket(null,(new BlockPos(mc.player.posX,0, mc.player.posZ)));
+                placeBlockPacket(null,(new BlockPos(mc.player.posX,1, mc.player.posZ)));
 
         }
     }
     void placeBlockPacket(EnumFacing side, BlockPos pos) {
 
-        int targetBlockSlot = InventoryUtil.findObsidianSlot(false, true);
+        cont = true;
+        doPlace = true;
 
-        if (!(targetBlockSlot > 0 && targetBlockSlot < 9)) {
+        targetBlockSlot = -1;
 
-            MessageBus.sendClientPrefixMessage(ModuleManager.getModule(ColorMain.class).getDisabledColor() + "You are fucked...");
+        for (int i = 1; i < 10; i++) {
+
+            if (cont && mc.player.inventory.getStackInSlot(i) != ItemStack.EMPTY && mc.player.inventory.getStackInSlot(i).item instanceof ItemBlock) {
+
+                targetBlockSlot = i;
+
+                cont = false;
+
+            }
+
+        }
+
+
+        if (!(targetBlockSlot == -1)) {
+
+            if (mc.player.posY > 0){
+                MessageBus.sendClientPrefixMessage(ModuleManager.getModule(ColorMain.class).getDisabledColor() + "You are out of blocks... good luck!");
+            }
             doPlace = false;
 
+        } else {
+            doPlace = true;
         }
 
         if (doPlace) {
