d52afd4f63b57eeb60574c61cb4a16d4d33f39af
Doogie13 <jay.finbow13@gmail.com>
2021-07-29 12:26:24 +0100

    airjump lol

--- src/main/java/com/gamesense/client/module/modules/movement/AirJump.java
+++ src/main/java/com/gamesense/client/module/modules/movement/AirJump.java
@@ -1,15 +1,28 @@
 package com.gamesense.client.module.modules.movement;
 
+import com.gamesense.api.setting.values.IntegerSetting;
+import com.gamesense.api.setting.values.ModeSetting;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
+
+import java.util.Arrays;
 
 @Module.Declaration(name = "AirJump", category = Category.Movement)
 public class AirJump extends Module {
 
+    ModeSetting mode = registerMode("Mode", Arrays.asList("Single", "Repeat"), "Single");
+    IntegerSetting repeat = registerInteger("Repeat", 19,1,20, () -> mode.getValue().equalsIgnoreCase("Repeat"));
+
     @Override
     public void onUpdate() {
-        if (mc.gameSettings.keyBindJump.isPressed()) {
+        if (mode.getValue().equalsIgnoreCase("Single")){
+            if (mc.gameSettings.keyBindJump.isPressed()) {
+                mc.player.jump();
+            }
+        } else if (mode.getValue().equalsIgnoreCase("Repeat") && mc.player.ticksExisted % repeat.getValue() == 0 && mc.gameSettings.keyBindJump.isKeyDown()) {
+
             mc.player.jump();
+
         }
     }
 }
