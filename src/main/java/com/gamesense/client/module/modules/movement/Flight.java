a266e4227c5bffb5918a5740472b7728e2d08473
Doogie13 <jay.finbow13@gmail.com>
2021-08-25 21:32:27 +0100

    lol more stuff

--- src/main/java/com/gamesense/client/module/modules/movement/Flight.java
+++ src/main/java/com/gamesense/client/module/modules/movement/Flight.java
@@ -2,15 +2,21 @@ package com.gamesense.client.module.modules.movement;
 
 import com.gamesense.api.event.events.PlayerMoveEvent;
 import com.gamesense.api.setting.values.DoubleSetting;
+import com.gamesense.api.setting.values.ModeSetting;
 import com.gamesense.api.util.world.MotionUtil;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 
+import java.util.Arrays;
+
 @Module.Declaration(name = "Flight", category = Category.Movement)
 public class Flight extends Module {
 
+    float flyspeed;
+
+    ModeSetting mode = registerMode("Mode", Arrays.asList("Vanilla", "Static", "Packet"), "Static");
     DoubleSetting speed = registerDouble("Speed", 2,0,10);
     DoubleSetting ySpeed = registerDouble("Y Speed", 1,0,10);
     DoubleSetting glideSpeed = registerDouble("Glide Speed", 0,-10,10);
@@ -18,29 +24,46 @@ public class Flight extends Module {
     @EventHandler
     private final Listener<PlayerMoveEvent> playerMoveEventListener = new Listener<>(event -> {
 
-        if (mc.gameSettings.keyBindJump.isKeyDown()) {
+        if (mode.getValue().equalsIgnoreCase("Static")){
+            if (mc.gameSettings.keyBindJump.isKeyDown()) {
 
-            event.setY(ySpeed.getValue());
+                event.setY(ySpeed.getValue());
 
-        } else if (mc.gameSettings.keyBindSneak.isKeyDown()) {
+            } else if (mc.gameSettings.keyBindSneak.isKeyDown()) {
 
-            event.setY(-ySpeed.getValue());
+                event.setY(-ySpeed.getValue());
 
-        } else {
+            } else {
 
-            event.setY(-glideSpeed.getValue());
+                event.setY(-glideSpeed.getValue());
 
-        }
+            }
 
-        if (MotionUtil.isMoving(mc.player)){
-            MotionUtil.setSpeed(mc.player, speed.getValue());
-        } else {
+            if (MotionUtil.isMoving(mc.player)) {
+                MotionUtil.setSpeed(mc.player, speed.getValue());
+            } else {
 
-            event.setX(0);
-            event.setZ(0);
+                event.setX(0);
+                event.setZ(0);
+
+            }
+        } else if (mode.getValue().equalsIgnoreCase("Vanilla")) {
+
+            mc.player.capabilities.setFlySpeed(speed.getValue().floatValue());
+            mc.player.capabilities.isFlying = true;
 
         }
 
     });
 
+    @Override
+    protected void onEnable() {
+        flyspeed = mc.player.capabilities.getFlySpeed();
+    }
+
+    @Override
+    protected void onDisable() {
+        mc.player.capabilities.setFlySpeed(flyspeed);
+        mc.player.capabilities.isFlying = false;
+    }
 }
