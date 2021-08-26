49ecdc8d5324c552a5050cc4020e607327550f0b
Doogie13 <jay.finbow13@gmail.com>
2021-08-26 19:48:37 +0100

    hotfix viewmodel works now because why did techale have to merge KA branch

--- src/main/java/com/gamesense/client/module/modules/render/ViewModel.java
+++ src/main/java/com/gamesense/client/module/modules/render/ViewModel.java
@@ -7,8 +7,6 @@ import com.gamesense.api.setting.values.IntegerSetting;
 import com.gamesense.api.setting.values.ModeSetting;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
-import com.gamesense.client.module.ModuleManager;
-import com.gamesense.client.module.modules.combat.KillAura;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.client.renderer.GlStateManager;
@@ -39,33 +37,27 @@ public class ViewModel extends Module {
     IntegerSetting xLeftRotate = registerInteger("Left X Rotate", 0, 0, 180);
     IntegerSetting yLeftRotate = registerInteger("Left Y Rotate", 0, 0, 180);
     IntegerSetting zLeftRotate = registerInteger("Left Z Rotate", 0, 0, 180);
-    public DoubleSetting xRight = registerDouble("Right X", 0.0, -2.0, 2.0);
-    public DoubleSetting yRight = registerDouble("Right Y", 0.2, -2.0, 2.0);
-    public DoubleSetting zRight = registerDouble("Right Z", -1.2, -2.0, 2.0);
-    public IntegerSetting xRightRotate = registerInteger("Right X Rotate", 0, 0, 360);
-    public IntegerSetting yRightRotate = registerInteger("Right Y Rotate", 0, 0, 360);
-    public IntegerSetting zRightRotate = registerInteger("Right Z Rotate", 0, 0, 360);
-    public DoubleSetting xScaleRight = registerDouble("Right X Scale", 1, 0, 3);
-    public DoubleSetting yScaleRight = registerDouble("Right Y Scale", 1, 0, 3);
-    public DoubleSetting zScaleRight = registerDouble("Right Z Scale", 1, 0, 3);
+    DoubleSetting xRight = registerDouble("Right X", 0.0, -2.0, 2.0);
+    DoubleSetting yRight = registerDouble("Right Y", 0.2, -2.0, 2.0);
+    DoubleSetting zRight = registerDouble("Right Z", -1.2, -2.0, 2.0);
+    IntegerSetting xRightRotate = registerInteger("Right X Rotate", 0, 0, 360);
+    IntegerSetting yRightRotate = registerInteger("Right Y Rotate", 0, 0, 360);
+    IntegerSetting zRightRotate = registerInteger("Right Z Rotate", 0, 0, 360);
+    DoubleSetting xScaleRight = registerDouble("Right X Scale", 1, 0, 3);
+    DoubleSetting yScaleRight = registerDouble("Right Y Scale", 1, 0, 3);
+    DoubleSetting zScaleRight = registerDouble("Right Z Scale", 1, 0, 3);
     DoubleSetting fov = registerDouble("Item FOV", 130, 70, 200);
-
-    KillAura ka = ModuleManager.getModule(KillAura.class);
 
     @SuppressWarnings("unused")
     @EventHandler
     private final Listener<TransformSideFirstPersonEvent> eventListener = new Listener<>(event -> {
         if (type.getValue().equalsIgnoreCase("Value") || type.getValue().equalsIgnoreCase("Both")) {
             if (event.getEnumHandSide() == EnumHandSide.RIGHT) {
-                if (!ka.animation.getValue()){
-                    GlStateManager.scale(xScaleRight.getValue(), yScaleRight.getValue(), zScaleRight.getValue());
-                }
+                GlStateManager.scale(xScaleRight.getValue(), yScaleRight.getValue(), zScaleRight.getValue());
                 glRotatef(xRightRotate.getValue(), 1, 0, 0);
                 glRotatef(yRightRotate.getValue(), 0, 1, 0);
                 glRotatef(zRightRotate.getValue(), 0, 0, 1);
-                if (!ka.animation.getValue()){
-                    GlStateManager.translate(xRight.getValue(), yRight.getValue(), zRight.getValue());
-                }
+                GlStateManager.translate(xRight.getValue(), yRight.getValue(), zRight.getValue());
             } else if (event.getEnumHandSide() == EnumHandSide.LEFT) {
                 GlStateManager.scale(xScaleLeft.getValue(), yScaleLeft.getValue(), zScaleLeft.getValue());
                 GlStateManager.translate(xLeft.getValue(), yLeft.getValue(), zLeft.getValue());
