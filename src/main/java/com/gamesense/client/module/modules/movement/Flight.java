a4e9ebefd07cbd93e84ffdfe8f382558707ade13
Doogie13 <jay.finbow13@gmail.com>
2021-08-31 21:13:13 +0100

    damage modes?

--- src/main/java/com/gamesense/client/module/modules/movement/Flight.java
+++ src/main/java/com/gamesense/client/module/modules/movement/Flight.java
@@ -10,6 +10,7 @@ import com.gamesense.client.module.Module;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.network.play.client.CPacketPlayer;
+
 import java.util.Arrays;
 
 @Module.Declaration(name = "Flight", category = Category.Movement)
@@ -19,14 +20,15 @@ public class Flight extends Module {
 
     ModeSetting mode = registerMode("Mode", Arrays.asList("Vanilla", "Static", "Packet", "Damage"), "Static");
     BooleanSetting antiKick = registerBoolean("Anti Kick", true, () -> mode.getValue().equalsIgnoreCase("Packet"));
-    DoubleSetting speed = registerDouble("Speed", 2,0,10);
-    DoubleSetting ySpeed = registerDouble("Y Speed", 1,0,10);
-    DoubleSetting glideSpeed = registerDouble("Glide Speed", 0,-10,10);
+    ModeSetting damage = registerMode("Damage Mode", Arrays.asList("LB", "WI"), "WI");
+    DoubleSetting speed = registerDouble("Speed", 2, 0, 10);
+    DoubleSetting ySpeed = registerDouble("Y Speed", 1, 0, 10);
+    DoubleSetting glideSpeed = registerDouble("Glide Speed", 0, -10, 10);
 
     @EventHandler
     private final Listener<PlayerMoveEvent> playerMoveEventListener = new Listener<>(event -> {
 
-        if (mode.getValue().equalsIgnoreCase("Static")){
+        if (mode.getValue().equalsIgnoreCase("Static")) {
             if (mc.gameSettings.keyBindJump.isKeyDown()) {
 
                 event.setY(ySpeed.getValue());
@@ -60,17 +62,17 @@ public class Flight extends Module {
 
             if (mc.gameSettings.keyBindSneak.isKeyDown() && !mc.gameSettings.keyBindJump.isKeyDown()) {
                 mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX + mc.player.motionX, mc.player.posY - 0.0624, mc.player.posZ + mc.player.motionZ, mc.player.rotationYaw, mc.player.rotationPitch, false));
-                mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX,mc.player.posY + 69420, mc.player.posZ,mc.player.rotationYaw,mc.player.rotationPitch, false));
+                mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX, mc.player.posY + 69420, mc.player.posZ, mc.player.rotationYaw, mc.player.rotationPitch, false));
             }
             if (mc.gameSettings.keyBindJump.isKeyDown()) {
                 mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX + mc.player.motionX, mc.player.posY + 0.0624, mc.player.posZ + mc.player.motionZ, mc.player.rotationYaw, mc.player.rotationPitch, false));
-                mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX,mc.player.posY + 69420, mc.player.posZ,mc.player.rotationYaw,mc.player.rotationPitch, false));
+                mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX, mc.player.posY + 69420, mc.player.posZ, mc.player.rotationYaw, mc.player.rotationPitch, false));
 
             }
             if (mc.gameSettings.keyBindForward.isKeyDown() || mc.gameSettings.keyBindBack.isKeyDown() || mc.gameSettings.keyBindLeft.isKeyDown() || mc.gameSettings.keyBindRight.isKeyDown()) {
                 double[] dir = MotionUtil.forward(0.0624);
                 mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX + (dir[0]), mc.player.posY, mc.player.posZ + (dir[1]), false));
-                mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX,mc.player.posY + 69420, mc.player.posZ,mc.player.rotationYaw,mc.player.rotationPitch, false));
+                mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX, mc.player.posY + 69420, mc.player.posZ, mc.player.rotationYaw, mc.player.rotationPitch, false));
 
             }
             if (antiKick.getValue() && mc.player.ticksExisted % 4 == 0) {
@@ -117,11 +119,23 @@ public class Flight extends Module {
 
     public void damage() {
 
-        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 3.1, mc.player.posZ, false)); // send the player up
-        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.05, mc.player.posZ, false));
-        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, true)); // set onground to true and deal damage
+        if (damage.getValue().equalsIgnoreCase("WI")) {
+            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 3.1, mc.player.posZ, false)); // send the player up
+            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.05, mc.player.posZ, false));
+            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, true)); // set onground to true and deal damage
 
-        mc.player.motionY = -5; // go down fast (idk if will help at all)
+            mc.player.motionY = -5; // go down fast (idk if will help at all)}
+
+        } else {
+
+            for (int i = 0; i > 64; i++) {
+                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.049, mc.player.posZ, false));
+                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+            }
+
+            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.1, mc.player.posZ, true));
+
+        }
 
     }
 
