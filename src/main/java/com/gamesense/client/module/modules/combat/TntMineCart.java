d54f3ccb2f9be6cd60119eca375408741a3c7c67
Doogie13 <jay.finbow13@gmail.com>
2021-09-09 19:30:26 +0100

    TntMineCart will now do nothing but more so

--- src/main/java/com/gamesense/client/module/modules/combat/TntMineCart.java
+++ src/main/java/com/gamesense/client/module/modules/combat/TntMineCart.java
@@ -6,6 +6,7 @@ import com.gamesense.api.setting.values.IntegerSetting;
 import com.gamesense.api.util.player.PlacementUtil;
 import com.gamesense.api.util.player.PlayerUtil;
 import com.gamesense.api.util.player.RotationUtil;
+import com.gamesense.api.util.world.BlockUtil;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
 import me.zero.alpine.listener.EventHandler;
@@ -23,6 +24,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec2f;
 
+import java.util.Objects;
+
 @Module.Declaration(name = "TntMineCart", category = Category.Combat)
 public class TntMineCart extends Module {
 
@@ -37,6 +40,12 @@ public class TntMineCart extends Module {
     int phase;
 
     Vec2f rot;
+
+    @Override
+    protected void onEnable() {
+        phase = 0;
+    }
+
     @EventHandler
     private final Listener<PacketEvent.Send> sendListener = new Listener<>(event -> {
 
@@ -76,37 +85,18 @@ public class TntMineCart extends Module {
 
     @Override
     public void onUpdate() {
+        if (target != null){
+            if (mc.world.isAirBlock(new BlockPos(target.getPositionVector())))
+                phase = 0;
+            else
+                phase = 1;
 
-        if (mc.world.isAirBlock(new BlockPos(target.getPositionVector())))
-            phase = 0;
-        else
-            phase = 1;
+            switch (phase) {
+                case 0: {
 
-        switch (phase) {
-            case 0: {
-
-                target = getTarget();
-                oldSlot = mc.player.inventory.currentItem;
-                slot = getSlot(Blocks.ACTIVATOR_RAIL);
-
-                if (!silent.getValue())
-                    mc.player.inventory.changeCurrentItem(slot);
-
-                else
-                    mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
-
-                PlacementUtil.place(new BlockPos(target.getPositionVector()), EnumHand.MAIN_HAND, false); // we are already rotating with the listener
-
-                if (silent.getValue())
-                    mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
-
-            }
-            case 1: {
-
-                if (mc.player.ticksExisted % delay.getValue() == 0) {
                     target = getTarget();
                     oldSlot = mc.player.inventory.currentItem;
-                    slot = getSlot(Blocks.TNT);
+                    slot = getSlot(Blocks.ACTIVATOR_RAIL);
 
                     if (!silent.getValue())
                         mc.player.inventory.changeCurrentItem(slot);
@@ -114,15 +104,51 @@ public class TntMineCart extends Module {
                     else
                         mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
 
-                    mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(new BlockPos(target.getPositionVector()), EnumFacing.UP, EnumHand.MAIN_HAND, 0, 0, 0));
+                    PlacementUtil.place(new BlockPos(target.getPositionVector()), EnumHand.MAIN_HAND, false); // we are already rotating with the listener
 
                     if (silent.getValue())
                         mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
-                }
 
+                }
+                case 1: {
+
+                    if (mc.player.ticksExisted % delay.getValue() == 0) {
+                        target = getTarget();
+                        oldSlot = mc.player.inventory.currentItem;
+                        slot = getSlot(Blocks.TNT);
+
+                        if (!silent.getValue())
+                            mc.player.inventory.changeCurrentItem(slot);
+
+                        else
+                            mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+
+                        mc.getConnection().sendPacket(new CPacketPlayerTryUseItemOnBlock(new BlockPos(target.getPositionVector()), EnumFacing.NORTH, EnumHand.MAIN_HAND, 0, 0, 0));
+
+                        if (silent.getValue())
+                            mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+                    }
+                }
+                case 2: {
+
+                    target = getTarget();
+                    oldSlot = mc.player.inventory.currentItem;
+                    slot = getSlot(Blocks.REDSTONE_TORCH);
+
+                    if (!silent.getValue())
+                        mc.player.inventory.changeCurrentItem(slot);
+
+                    else
+                        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+
+                    PlacementUtil.place(new BlockPos(target.getPositionVector()).add(0,1,0), EnumHand.MAIN_HAND, false);
+
+                    if (silent.getValue())
+                        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+
+                }
             }
         }
-
     }
 
     public EntityPlayer getTarget() {
