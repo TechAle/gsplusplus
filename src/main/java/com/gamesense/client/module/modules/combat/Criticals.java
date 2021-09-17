64c5386f42399db79f5ab29dab2aa046732f91d3
Doogie13 <jay.finbow13@gmail.com>
2021-09-17 17:26:31 +0100

    Criticals and NoSwing updates Added NoSwing > Client mode Fixed Criticals criting crystals

--- src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
+++ src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
@@ -8,9 +8,12 @@ import com.gamesense.client.module.Module;
 import com.gamesense.client.module.ModuleManager;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketUseEntity;
+
+import java.util.Objects;
 
 @Module.Declaration(name = "Criticals", category = Category.Combat)
 public class Criticals extends Module {
@@ -18,19 +21,23 @@ public class Criticals extends Module {
     public BooleanSetting onlyAura = registerBoolean("Only KillAura Target", false);
     BooleanSetting allowWater = registerBoolean("In Water", false);
 
+    private Entity EntityEnderCrystal;
+
     @EventHandler
     private final Listener<PacketEvent.Send> listener = new Listener<>(event -> {
-        if (!(ModuleManager.getModule(AutoCrystalRewrite.class).bestBreak.crystal == null)){
-            if (event.getPacket() instanceof CPacketUseEntity && (!mc.player.isInWater() || !allowWater.getValue())) {
-                if (((CPacketUseEntity) event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && mc.player.onGround) {
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.11, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.1100013579, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.3579E-6, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer());
+        try {
+            if (!(ModuleManager.getModule(AutoCrystalRewrite.class).bestBreak.crystal == null)) {
+                if (event.getPacket() instanceof CPacketUseEntity && (!mc.player.isInWater() || !allowWater.getValue())) {
+                    if (((CPacketUseEntity) event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && mc.player.onGround && ((CPacketUseEntity) event.getPacket()).getEntityFromWorld(mc.world).isEntityEqual(EntityEnderCrystal)) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.11, mc.player.posZ, false));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.1100013579, mc.player.posZ, false));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.3579E-6, mc.player.posZ, false));
+                        mc.player.connection.sendPacket(new CPacketPlayer());
 
+                    }
                 }
             }
-        }
+        } catch (NullPointerException ignored){}
     });
 
 }
