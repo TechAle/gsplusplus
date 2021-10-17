54d7def26c256cfe07a1f77653d916ed24ffee2c
Jacob Herd <toxicaven@disroot.org>
2021-10-17 19:39:37 -0400

    Revert "Configs"

--- src/main/java/com/gamesense/client/command/commands/SaveConfigCommand.java
+++ src/main/java/com/gamesense/client/command/commands/SaveConfigCommand.java
@@ -1,6 +1,5 @@
 package com.gamesense.client.command.commands;
 
-import com.gamesense.api.config.ProfileManager;
 import com.gamesense.api.config.SaveConfig;
 import com.gamesense.api.util.misc.MessageBus;
 import com.gamesense.client.command.Command;
@@ -10,11 +9,10 @@ import com.gamesense.client.command.Command;
  * @since 1/1/2020
  */
 
-@Command.Declaration(name = "SaveConfig", syntax = "saveconfig", alias = {"saveconfig", "reloadconfig", "saveconfiguration"})
+@Command.Declaration(name = "SaveConfig", syntax = "saveconfig", alias = {"saveconfig", "config", "saveconfiguration"})
 public class SaveConfigCommand extends Command {
 
     public void onCommand(String command, String[] message) {
-        SaveConfig.setProfile(ProfileManager.getCurrentProfile());
         SaveConfig.init();
         MessageBus.sendCommandMessage("Config saved!", true);
     }
