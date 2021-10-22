c02b4aba01cf9701cf7f0b216033f6f588d8b4c8
Doogie13 <jay.finbow13@gmail.com>
2021-10-22 10:27:04 +0100

    LongJump cleanup

--- src/main/java/com/gamesense/client/module/modules/movement/LongJump.java
+++ src/main/java/com/gamesense/client/module/modules/movement/LongJump.java
@@ -24,27 +24,31 @@ import java.util.Arrays;
 @Module.Declaration(name = "LongJump", category = Category.Movement)
 public class LongJump extends Module {
 
-    ModeSetting mode = registerMode("mode", Arrays.asList("Bhop", "Peak", "Ground", "Velocity", "Continuous"), "Peak");
+    ModeSetting mode = registerMode("mode", Arrays.asList("BHop", "Peak", "Ground", "Velocity", "Continuous"), "Peak");
 
-    DoubleSetting speed = registerDouble("strafeSpeed", 2.15, 0, 10, () -> mode.getValue().equalsIgnoreCase("Bhop"));
+    // BHop
+    DoubleSetting speed = registerDouble("BHop speed", 2.15, 0, 10, () -> mode.getValue().equalsIgnoreCase("BHop"));
 
-
-
+    // Peak
     DoubleSetting farSpeed = registerDouble("Peak Speed", 1, 0, 10, () -> mode.getValue().equalsIgnoreCase("Peak"));
     IntegerSetting farAccel = registerInteger("Peak Acceleration", 0, 1, 5, () -> mode.getValue().equalsIgnoreCase("Peak"));
     DoubleSetting initialFar = registerDouble("Peak Hop Speed", 1, 0, 10, () -> mode.getValue().equalsIgnoreCase("Peak"));
 
+    // Velocity
     BooleanSetting jump = registerBoolean("Jump", true, () -> mode.getValue().equalsIgnoreCase("Velocity"));
     DoubleSetting jumpHeightVelo = registerDouble("Jump Height Velocity", 1,0,10, () -> mode.getValue().equalsIgnoreCase("Velocity"));
     BooleanSetting allowY = registerBoolean("Velocity Multiply", true, () -> mode.getValue().equalsIgnoreCase("Velocity"));
     DoubleSetting xzvelocity = registerDouble("XZ Velocity Multiplier", 0.1,0,5, () -> mode.getValue().equalsIgnoreCase("Velocity"));
     DoubleSetting yvelocity = registerDouble("Y Velocity Multiplier", 0.1,0,2, () -> mode.getValue().equalsIgnoreCase("Velocity"));
 
+    // Continuous
     DoubleSetting speedFactor = registerDouble("Factor Acceleration", 0.3,0,3, () -> mode.getValue().equalsIgnoreCase("Continuous"));
     DoubleSetting factorMax = registerDouble("Factor Max", 0,0,50, () -> mode.getValue().equalsIgnoreCase("Continuous"));
 
-    DoubleSetting normalSpeed = registerDouble("Normal Speed", 3,0,10, () -> mode.getValue().equalsIgnoreCase("Ground"));
+    // Ground
+    DoubleSetting normalSpeed = registerDouble("Ground Speed", 3,0,10, () -> mode.getValue().equalsIgnoreCase("Ground"));
 
+    // Other
     BooleanSetting lagback = registerBoolean("Disable On LagBack", false);
     DoubleSetting jumpHeight = registerDouble("jumpHeight", 0.41, 0, 1);
 
@@ -76,11 +80,52 @@ public class LongJump extends Module {
     private final Listener<PacketEvent.Receive> receiveListener = new Listener<>(event -> {
         if (event.getPacket() instanceof SPacketPlayerPosLook && lagback.getValue())
             disable();
+
+        if ((event.getPacket() instanceof SPacketExplosion || event.getPacket() instanceof SPacketEntityVelocity) && mc.player != null) {
+            if (mode.getValue().equals("Velocity")) {
+
+                double[] dir = MotionUtil.forward(1);
+
+                if (jump.getValue() && mc.player.onGround) {
+
+                    mc.player.jumpMovementFactor = jumpHeightVelo.getValue().floatValue();
+                    mc.player.motionY = jumpHeight.getValue();
+
+                }
+
+                velo = false;
+
+                if (event.getPacket() instanceof SPacketEntityVelocity){
+                    if (!(!allowY.getValue() && ((SPacketEntityVelocity) event.getPacket()).motionY > 0)) {
+
+                        ((SPacketEntityVelocity) event.getPacket()).motionY = ((int) (((SPacketEntityVelocity) event.getPacket()).motionY * yvelocity.getValue()));
+                        ((SPacketEntityVelocity) event.getPacket()).motionX = ((int) (((SPacketEntityVelocity) event.getPacket()).motionX * xzvelocity.getValue() * dir[0]));
+                        ((SPacketEntityVelocity) event.getPacket()).motionZ = ((int) (((SPacketEntityVelocity) event.getPacket()).motionZ * xzvelocity.getValue() * dir[1]));
+
+                    }
+                }
+                if (event.getPacket() instanceof SPacketExplosion){
+                    if (!(!allowY.getValue() && ((SPacketExplosion) event.getPacket()).motionY > 0)) {
+
+                        ((SPacketExplosion) event.getPacket()).motionY = ((int) (((SPacketExplosion) event.getPacket()).motionY * yvelocity.getValue()));;
+                        ((SPacketExplosion) event.getPacket()).motionX = ((int) (((SPacketExplosion) event.getPacket()).motionX * xzvelocity.getValue() * dir[0]));
+                        ((SPacketExplosion) event.getPacket()).motionZ = ((int) (((SPacketExplosion) event.getPacket()).motionZ * xzvelocity.getValue() * dir[1]));
+
+                    }
+                }
+
+            } else {
+
+                velo = true;
+
+            }
+        }
+
     });
 
     @EventHandler
     private final Listener<PlayerMoveEvent> playerMoveEventListener = new Listener<>(event -> {
-        if (mode.getValue().equals("Bhop")) {
+        if (mode.getValue().equals("BHop")) {
             if (mc.player.isInLava() || mc.player.isInWater() || mc.player.isOnLadder() || mc.player.isInWeb || Anchor.active) {
                 return;
             }
@@ -161,50 +206,6 @@ public class LongJump extends Module {
 
         }
     }
-
-    @EventHandler
-    private final Listener<PacketEvent.Receive> receiveListenerTwo = new Listener<>(event -> {
-
-        if ((event.getPacket() instanceof SPacketExplosion || event.getPacket() instanceof SPacketEntityVelocity) && mc.player != null) {
-            if (mode.getValue().equals("Velocity")) {
-
-                double[] dir = MotionUtil.forward(1);
-
-                if (jump.getValue() && mc.player.onGround) {
-
-                    mc.player.jumpMovementFactor = jumpHeightVelo.getValue().floatValue();
-                    mc.player.motionY = jumpHeight.getValue();
-
-                }
-
-                velo = false;
-
-                if (event.getPacket() instanceof SPacketEntityVelocity){
-                    if (!(!allowY.getValue() && ((SPacketEntityVelocity) event.getPacket()).motionY > 0)) {
-
-                        ((SPacketEntityVelocity) event.getPacket()).motionY = ((int) (((SPacketEntityVelocity) event.getPacket()).motionY * yvelocity.getValue()));
-                        ((SPacketEntityVelocity) event.getPacket()).motionX = ((int) (((SPacketEntityVelocity) event.getPacket()).motionX * xzvelocity.getValue() * dir[0]));
-                        ((SPacketEntityVelocity) event.getPacket()).motionZ = ((int) (((SPacketEntityVelocity) event.getPacket()).motionZ * xzvelocity.getValue() * dir[1]));
-
-                    }
-                }
-                if (event.getPacket() instanceof SPacketExplosion){
-                    if (!(!allowY.getValue() && ((SPacketExplosion) event.getPacket()).motionY > 0)) {
-
-                        ((SPacketExplosion) event.getPacket()).motionY = ((int) (((SPacketExplosion) event.getPacket()).motionY * yvelocity.getValue()));;
-                        ((SPacketExplosion) event.getPacket()).motionX = ((int) (((SPacketExplosion) event.getPacket()).motionX * xzvelocity.getValue() * dir[0]));
-                        ((SPacketExplosion) event.getPacket()).motionZ = ((int) (((SPacketExplosion) event.getPacket()).motionZ * xzvelocity.getValue() * dir[1]));
-
-                    }
-                }
-
-            } else {
-
-                velo = true;
-
-            }
-        }
-    });
 }
 
 
