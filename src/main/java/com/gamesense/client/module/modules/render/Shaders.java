f6493034c0e3dfc13135ba5b3101ffad992aaea9
TechAle <alessandro.condello.email@gmail.com>
2021-10-23 13:37:26 +0200

    Added europa shaders and fixed shaders crash

--- src/main/java/com/gamesense/client/module/modules/render/Shaders.java
+++ src/main/java/com/gamesense/client/module/modules/render/Shaders.java
@@ -4,26 +4,27 @@ import com.gamesense.api.event.events.RenderEvent;
 import com.gamesense.api.setting.values.BooleanSetting;
 import com.gamesense.api.setting.values.ColorSetting;
 import com.gamesense.api.setting.values.DoubleSetting;
+import com.gamesense.api.setting.values.ModeSetting;
 import com.gamesense.api.util.render.GSColor;
-import com.gamesense.api.util.render.shaders.impl.FlowShader;
-import com.gamesense.api.util.render.shaders.impl.GlowShader;
-import com.gamesense.api.util.render.shaders.impl.TestShader;
+import com.gamesense.api.util.render.shaders.impl.*;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 
 import java.awt.*;
+import java.util.Arrays;
 
 @Module.Declaration(name = "Shaders", category = Category.Render)
 public class Shaders extends Module {
 
-    BooleanSetting playerShader = registerBoolean("PlayerShader", false);
     BooleanSetting glowESP = registerBoolean("Glow ESP", false);
     ColorSetting color = registerColor("Color", new GSColor(255, 255, 255));
     DoubleSetting radius = registerDouble("Radius", 1, 0, 5);
     DoubleSetting quality = registerDouble("Quality", 1, 0, 5);
+    ModeSetting fillShader = registerMode("Fill Shader", Arrays.asList("Astral", "Aqua", "Red", "Smoke", "Test", "None"), "Astral");
 
+    public boolean renderTags = true;
     public void onWorldRender(RenderEvent event) {
 
         if (mc.world == null)
@@ -31,23 +32,47 @@ public class Shaders extends Module {
 
 
         GlStateManager.pushMatrix();
-        if (playerShader.getValue()) {
-            FlowShader.INSTANCE.startDraw(event.getPartialTicks());
-            mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
-            FlowShader.INSTANCE.stopDraw(Color.WHITE, 1f, 1f);
-        }
+        renderTags = false;
+
         if (glowESP.getValue()) {
             GlowShader.INSTANCE.startDraw(event.getPartialTicks());
             mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
             GlowShader.INSTANCE.stopDraw(color.getValue(), radius.getValue().floatValue(), quality.getValue().floatValue());
         }
-        /*
-        if (test.getValue()) {
-            TestShader.INSTANCE.startDraw(event.getPartialTicks());
-            mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
-            TestShader.INSTANCE.stopDraw(color.getValue(), 1f, 1f);
-        }*/
+
+        switch (fillShader.getValue()) {
+            case "Astral":
+                FlowShader.INSTANCE.startDraw(event.getPartialTicks());
+                mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
+                FlowShader.INSTANCE.stopDraw(Color.WHITE, 1f, 1f);
+                break;
+            case "Aqua":
+                AquaShader.INSTANCE.startDraw(event.getPartialTicks());
+                mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
+                AquaShader.INSTANCE.stopDraw(Color.WHITE, 1f, 1f);
+                break;
+            case "Red":
+                RedShader.INSTANCE.startDraw(event.getPartialTicks());
+                mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
+                RedShader.INSTANCE.stopDraw(Color.WHITE, 1f, 1f);
+                break;
+            case "Smoke":
+                SmokeShader.INSTANCE.startDraw(event.getPartialTicks());
+                mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
+                SmokeShader.INSTANCE.stopDraw(Color.WHITE, 1f, 1f);
+                break;
+            case "Test":
+                TestShader.INSTANCE.startDraw(event.getPartialTicks());
+                mc.world.loadedEntityList.stream().filter(e -> e instanceof EntityPlayer && e != mc.player).forEach(e -> mc.getRenderManager().renderEntityStatic(e, event.getPartialTicks(), true));
+                TestShader.INSTANCE.stopDraw(color.getValue(), 1f, 1f);
+                break;
+        }
+
+        renderTags = true;
         GlStateManager.popMatrix();
     }
+
+
+
 
 }
