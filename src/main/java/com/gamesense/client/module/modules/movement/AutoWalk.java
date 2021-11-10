7abb3607a45ff2919d407a7a648f2c68fd364627
Doogie13 <jay.finbow13@gmail.com>
2021-11-10 21:34:38 +0000

    autowalk should work??????? NoEntityTrace added food option for miningspoof

--- src/main/java/com/gamesense/client/module/modules/movement/AutoWalk.java
+++ src/main/java/com/gamesense/client/module/modules/movement/AutoWalk.java
@@ -1,19 +1,19 @@
 package com.gamesense.client.module.modules.movement;
 
-import com.gamesense.api.event.events.PlayerMoveEvent;
-import com.gamesense.api.util.player.PlayerUtil;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import org.lwjgl.input.Keyboard;
+import net.minecraft.client.settings.KeyBinding;
 
 @Module.Declaration(name = "AutoWalk", category = Category.Movement)
 public class AutoWalk extends Module {
 
     @Override
     public void onUpdate() {
-        mc.player.movementInput.moveForward = 1;
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
+    }
+
+    @Override
+    protected void onDisable() {
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
     }
 }
