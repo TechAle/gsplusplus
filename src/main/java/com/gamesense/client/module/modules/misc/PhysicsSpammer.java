9a371232c511320bd5e30ae4c60226644db8e9e8
Doogie13 <jay.finbow13@gmail.com>
2021-11-14 16:01:53 +0000

    debloat gui

--- src/main/java/com/gamesense/client/module/modules/misc/PhysicsSpammer.java
+++ src/main/java/com/gamesense/client/module/modules/misc/PhysicsSpammer.java
@@ -1,3 +1,4 @@
+/*
 package com.gamesense.client.module.modules.misc;
 
 import com.gamesense.api.setting.values.IntegerSetting;
@@ -70,4 +71,4 @@ public class PhysicsSpammer extends Module {
         int bound = Math.abs(maxDelay.getValue() - minDelay.getValue());
         delay = (bound == 0 ? 0 : random.nextInt(bound)) + Math.min(maxDelay.getValue(), minDelay.getValue());
     }
-}
+}*/
