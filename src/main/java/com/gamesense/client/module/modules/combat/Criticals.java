096bd415603670ac79c75062cf789e3f8fbd53c9
Doogie13 <jay.finbow13@gmail.com>
2021-12-06 21:22:09 +0000

    Damage Boost via added packets to the critical hits module for gamesense/plusplus

--- src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
+++ src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
@@ -5,6 +5,8 @@ import com.gamesense.api.setting.values.BooleanSetting;
 import com.gamesense.api.util.player.PlayerUtil;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
+import com.gamesense.client.module.ModuleManager;
+import com.gamesense.client.module.modules.movement.PlayerTweaks;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.entity.item.EntityEnderCrystal;
@@ -27,11 +29,11 @@ public class Criticals extends Module {
                     if (((CPacketUseEntity) event.getPacket()).getEntityFromWorld(mc.world) != null)
                         if (((CPacketUseEntity) event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK
                                 && !(mc.world.getEntityByID(Objects.requireNonNull(((CPacketUseEntity) event.getPacket()).getEntityFromWorld(mc.world)).getEntityId()) instanceof EntityEnderCrystal)) {
+                            ModuleManager.getModule(PlayerTweaks.class).pauseNoFallPacket = true; // so it works kek
                             // ^ not an end crystal since no reason to crit crystals
                             mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.11, mc.player.posZ, false));
                             mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.1100013579, mc.player.posZ, false));
                             mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.3579E-6, mc.player.posZ, false));
-                            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, mc.player.onGround));
 
                         }
                 }
