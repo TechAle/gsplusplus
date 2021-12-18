3d345d83f1aa9d65ca883e0380159ff0e55734a4
TechAle <alessandro.condello.email@gmail.com>
2021-12-18 13:59:40 +0100

    antiPing

---
+++ src/main/java/com/gamesense/client/module/modules/misc/AntiPing.java
@@ -0,0 +1,9 @@
+package com.gamesense.client.module.modules.misc;
+
+import com.gamesense.client.module.Category;
+import com.gamesense.client.module.Module;
+
+@Module.Declaration(name = "AntiPing", category = Category.Misc, enabled = true)
+public class AntiPing extends Module {
+
+}
